4db0752e971d0bd9acf91699c172b34223bf627d
Greg Ives <gives@financialforce.com>
2019-06-26 17:37:22 +0100

    Add Semantic UI

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// Semantic UI
+import SuiVue from 'semantic-ui-vue'
+import 'semantic-ui-css/semantic.min.css'
+
+Vue.use(SuiVue)
+
 Vue.config.productionTip = false
 
 new Vue({

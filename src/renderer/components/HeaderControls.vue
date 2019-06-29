4d9ca6813e270fd000a5ed1f8936910279e83d67
Greg Ives <gregives774@gmail.com>
2019-06-29 22:29:02 +0100

    Test Robot.js mouse control

--- src/renderer/components/HeaderControls.vue
+++ src/renderer/components/HeaderControls.vue
@@ -1,22 +1,47 @@
 <template>
-  <div class="bg-white border-bottom shadow-sm p-4">
-    <b-dropdown split text="Record" variant="danger" class="mr-2">
-      <b-dropdown-item href="#">Play from beginning</b-dropdown-item>
-      <b-dropdown-item href="#">Play from selected</b-dropdown-item>
-    </b-dropdown>
-    <b-dropdown split text="Play" variant="success" class="mr-2">
-      <b-dropdown-item href="#">Play from beginning</b-dropdown-item>
-      <b-dropdown-item href="#">Play from selected</b-dropdown-item>
-    </b-dropdown>
-    <b-dropdown split text="Stop" variant="info" class="mr-2">
-      <b-dropdown-item href="#">Play from beginning</b-dropdown-item>
-      <b-dropdown-item href="#">Play from selected</b-dropdown-item>
-    </b-dropdown>
-  </div>
+  <b-row no-gutters>
+    <b-col cols="auto">
+      <div class="bg-white border-bottom border-right shadow-sm p-4">
+        <b-dropdown split text="Record" variant="danger" class="mr-2">
+          <b-dropdown-item href="#">Play from beginning</b-dropdown-item>
+          <b-dropdown-item href="#">Play from selected</b-dropdown-item>
+        </b-dropdown>
+        <b-dropdown split text="Play" variant="success" class="mr-2">
+          <b-dropdown-item href="#">Play from beginning</b-dropdown-item>
+          <b-dropdown-item href="#">Play from selected</b-dropdown-item>
+        </b-dropdown>
+        <b-dropdown split text="Stop" variant="info">
+          <b-dropdown-item href="#">Play from beginning</b-dropdown-item>
+          <b-dropdown-item href="#">Play from selected</b-dropdown-item>
+        </b-dropdown>
+      </div>
+    </b-col>
+    <b-col>
+      <div class="bg-white border-bottom shadow-sm p-4">
+        <b-button @click="testMouse">Test mouse</b-button>
+      </div>
+    </b-col>
+  </b-row>
 </template>
 
 <script>
+const robot = require('robotjs')
+robot.setMouseDelay(2)
+
+const twoPI = Math.PI * 2.0
+const screenSize = robot.getScreenSize()
+const height = (screenSize.height / 2) - 10
+const width = screenSize.width
+
 export default {
-  name: 'header-controls'
+  name: 'header-controls',
+  methods: {
+    testMouse () {
+      for (let x = 0; x < width; x++) {
+        const y = height * Math.sin((twoPI * x) / width) + height
+        robot.moveMouse(x, y)
+      }
+    }
+  }
 }
 </script>

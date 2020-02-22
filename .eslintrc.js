1a7865b67f5f41a88093bcb9d97f70f598669506
Greg Ives <gregives774@gmail.com>
2020-02-22 23:36:31 +0000

    Upgrade dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,13 +14,5 @@ module.exports = {
   },
   plugins: [
     'html'
-  ],
-  'rules': {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  }
+  ]
 }
